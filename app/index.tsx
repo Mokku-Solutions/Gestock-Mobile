b913129f00997225cc0bb97c26de0d0f01739a6a
Tomas Padilla <padillatomasagustin@gmail.com>
2025-03-26 16:37:12 +0100

    develop - Base Project

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,32 @@
-import { Text, View } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function Index() {
 	return (
-		<View
-			style={{
-				flex: 1,
-				justifyContent: "center",
-				alignItems: "center",
-			}}
-		>
-			<Text>Gestock!</Text>
+		<View style={styles.container}>
+			<View>
+				<TextInput placeholder="Your course goal!"></TextInput>
+				<Button title="Add Goal!"></Button>
+			</View>
+			<View>
+				<Text
+					style={{
+						margin: 16,
+						borderWidth: 2,
+						borderColor: "red",
+						padding: 16,
+					}}
+				>
+					Gestock!
+				</Text>
+			</View>
 		</View>
 	);
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+});
